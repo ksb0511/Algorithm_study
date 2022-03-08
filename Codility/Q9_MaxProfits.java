6cf6682092ebbfdf20b421d4e54ab2083bb977b4
ksb0511 <soobin796@gmail.com>
2022-03-08 14:25:40 +0900

    maxProfit Q9

---
+++ Codility/Q9_MaxProfits.java
@@ -0,0 +1,13 @@
+package Codility;
+
+public class Q9_MaxProfits {
+    public int solution(int[] A) { 
+        int min = (A.length > 0) ? A[0] : 0; 
+        int result = 0; 
+        for(int num : A) { 
+            if(num <= min) { min = num; } 
+            if(result < num - min) { result = num - min; } 
+        } 
+        return result; 
+    } 
+}
