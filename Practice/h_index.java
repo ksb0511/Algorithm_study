89581b11c2dcc8a7fe8fd31faf9e36cef5a4c2e0
ksb0511 <soobin796@gmail.com>
2022-04-19 11:22:17 +0900

    h_index

---
+++ Practice/h_index.java
@@ -0,0 +1,21 @@
+package Practice;
+
+import java.util.Arrays;
+
+public class h_index {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+ 
+        for (int i = 0; i < citations.length; i++) {
+            int h = citations.length - i;
+ 
+            if (citations[i] >= h) {
+                answer = h;
+                break;
+            }
+        }
+ 
+        return answer;
+    }
+}
