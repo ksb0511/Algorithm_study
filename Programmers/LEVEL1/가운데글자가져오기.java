daf3e6054279bbcb1042bb494ae6292a89436aaa
subeen Kang <soobin796@gmail.com>
2022-11-10 22:11:55 +0900

    Create 가운데글자가져오기.java

---
+++ Programmers/LEVEL1/가운데글자가져오기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String s) {
+        if(s.length()%2==0){
+            return String.valueOf(s.charAt(s.length()/2-1))+String.valueOf(s.charAt(s.length()/2));
+        }else {
+            return String.valueOf(s.charAt(s.length()/2));
+        }
+    }
+}
