1c89271451ec32b5a845928ff61a7309db305859
subeen Kang <soobin796@gmail.com>
2022-11-10 22:11:38 +0900

    Delete 가운데글자가져오기.java

--- Programmers/LEVE1/가운데글자가져오기.java
+++
@@ -1,9 +0,0 @@
-class Solution {
-    public String solution(String s) {
-        if(s.length()%2==0){
-            return String.valueOf(s.charAt(s.length()/2-1))+String.valueOf(s.charAt(s.length()/2));
-        }else {
-            return String.valueOf(s.charAt(s.length()/2));
-        }
-    }
-}
